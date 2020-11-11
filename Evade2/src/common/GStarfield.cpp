13d4bf3672f5528c789badf29f592340ea04c93f
jaygarcia <jesusdgarcia@gmail.com>
2020-11-11 13:45:18 -0500

    Starfield updates

--- Evade2/src/common/GStarfield.cpp
+++ Evade2/src/common/GStarfield.cpp
@@ -4,54 +4,212 @@
 
 #include "GStarfield.h"
 #include "GCamera.h"
+//
+//GStarfield::GStarfield() : BPlayfield() {
+//  for (TInt i = 0; i < NUM_STARS; i++) {
+//    InitStar(i);
+//  }
+//}
+//
+//GStarfield::~GStarfield() noexcept {
+//
+//}
+//
+//void GStarfield::Render() {
+//  gDisplay.renderBitmap->Clear(0);
+//  TFloat cz = gCamera->z,
+//         sw = TFloat(SCREEN_WIDTH),
+//         sh = TFloat(SCREEN_HEIGHT);
+//
+//  for (int i = 0; i < NUM_STARS; i++) {
+//    TFloat zz = (mStarZ[i] - cz) * 2;
+//    if (zz < 0) {
+//      InitStar(i);
+//      zz = (mStarZ[i] - cz) * 2;
+//    }
+//    TFloat ratioX = sw / (zz + sw);
+//    TFloat ratioY = sh / (zz + sh);
+//    TFloat x      = (sw / 2) - (mStarX[i] - gCamera->x) * ratioX;
+//    TFloat y      = (sh / 2) - (mStarY[i] - gCamera->y) * ratioY;
+//
+//    if (x < 0) {
+////      printf("InitStar x %f < 0\n", x);
+//      InitStar(i);
+//    } else if (x > sw) {
+////      printf("InitStar x %f > %f\n", x, sw);
+//      InitStar(i);
+//    } else if (y < 0) {
+////      printf("InitStar y %f < 0\n", y);
+//      InitStar(i);
+//    } else if (y > sh) {
+////      printf("InitStar y %f > %f\n", y, sh);
+//      InitStar(i);
+//    }
+////    printf("Plot %f,%f\n", x, y);
+//    gDisplay.renderBitmap->SafeWritePixel(x, y, COLOR_STAR);
+//  }
+//}
+//
+//void GStarfield::InitStar(TInt aIndex) {
+//  mStarX[aIndex] = TFloat(256) - Random(0, 512) + gCamera->x;
+//  mStarY[aIndex] = TFloat(256) - Random(0, 512) + gCamera->y;
+//  mStarZ[aIndex] = gCamera->z + Random(200, 512);
+//}
+//
 
-GStarfield::GStarfield() : BPlayfield() {
+GStarfield::GStarfield() : BPlayfield(){
+  mCurrSpeed = 10;
+  mMinSpeed = 1;
+  mBoostSpeed = EFalse;
+  mWarp = EFalse;
+
   for (TInt i = 0; i < NUM_STARS; i++) {
-    InitStar(i);
+    InitStar(i, 0);
   }
-}
-
-GStarfield::~GStarfield() noexcept {
-
 }
 
 void GStarfield::Render() {
   gDisplay.renderBitmap->Clear(0);
-  TFloat cz = gCamera->z,
-         sw = TFloat(SCREEN_WIDTH),
-         sh = TFloat(SCREEN_HEIGHT);
 
-  for (int i = 0; i < NUM_STARS; i++) {
-    TFloat zz = (mStarZ[i] - cz) * 2;
-    if (zz < 0) {
-      InitStar(i);
-      zz = (mStarZ[i] - cz) * 2;
-    }
-    TFloat ratioX = sw / (zz + sw);
-    TFloat ratioY = sh / (zz + sh);
-    TFloat x      = (sw / 2) - (mStarX[i] - gCamera->x) * ratioX;
-    TFloat y      = (sh / 2) - (mStarY[i] - gCamera->y) * ratioY;
 
-    if (x < 0) {
-//      printf("InitStar x %f < 0\n", x);
-      InitStar(i);
-    } else if (x > sw) {
-//      printf("InitStar x %f > %f\n", x, sw);
-      InitStar(i);
-    } else if (y < 0) {
-//      printf("InitStar y %f < 0\n", y);
-      InitStar(i);
-    } else if (y > sh) {
-//      printf("InitStar y %f > %f\n", y, sh);
-      InitStar(i);
+  TFloat travelX = 0,
+      travelY = 0,
+      travelZ = 0;
+
+  // Todo: this needs to be in Player.cpp or GGame.cpp
+  TBool jsRight = gControls.IsPressed(JOYRIGHT),
+      jsLeft = gControls.IsPressed(JOYLEFT),
+      jsUp = gControls.IsPressed(JOYUP),
+      jsDown = gControls.IsPressed(JOYDOWN),
+      jsRButton = gControls.IsPressed(BUTTONR),
+      jsLButton = gControls.IsPressed(BUTTONL);
+
+
+  //Todo: ROTATE via L or R Buttons in Z dimension
+  if (gGame->GetState() == GAME_STATE_GAME) {
+    // rotate
+    if (jsUp) {
+      travelY = .015;
     }
-//    printf("Plot %f,%f\n", x, y);
-    gDisplay.renderBitmap->SafeWritePixel(x, y, COLOR_STAR);
+
+    if (jsDown) {
+      travelY = -.015;
+    }
+
+    if (jsLeft) {
+      travelZ = -.015;
+    }
+
+    if (jsRight) {
+      travelZ = .015;
+    }
+
+    if (jsLButton) {
+      printf("TODO: Rotate stars & play field on L button\n");
+      travelX = .02;
+    }
+    if (jsRButton) {
+      printf("TODO: Rotate stars & play field on R button\n");
+      travelX = -.02;
+    }
+
   }
-}
+  //TODO: Investigate @Jay! If you turn travelZ above to travelX, the star field rotates!
+  if (gCamera->vz == CAMERA_WARP_VZ) {
+    mBoostSpeed = ETrue;
+  }
+  else {
+    mBoostSpeed = EFalse;
+  }
 
-void GStarfield::InitStar(TInt aIndex) {
-  mStarX[aIndex] = TFloat(256) - Random(0, 512) + gCamera->x;
-  mStarY[aIndex] = TFloat(256) - Random(0, 512) + gCamera->y;
-  mStarZ[aIndex] = gCamera->z + Random(200, 512);
+  // Loop through each star.
+  for (TInt16 i = 0; i < NUM_STARS; i++) {
+    stars[i].mZ -= mCurrSpeed;
+
+    if (travelY != 0) {
+      TFloat temp_y = stars[i].mY;
+      TFloat temp_z = stars[i].mZ;
+      stars[i].mY = temp_y * cos(travelY) - temp_z * sin(travelY);
+      stars[i].mZ = temp_z * cos(travelY) + temp_y * sin(travelY);
+    }
+
+    if (travelX != 0) {
+      TFloat temp_x = stars[i].mX;
+      TFloat temp_y = stars[i].mY;
+      stars[i].mX = temp_x * cos(travelX) - temp_y * sin(travelX);
+      stars[i].mY = temp_y * cos(travelX) + temp_x * sin(travelX);
+    }
+
+    if (travelZ != 0) {
+      TFloat temp_x = stars[i].mX;
+      TFloat temp_z = stars[i].mZ;
+      stars[i].mX = temp_x * cos(travelZ) - temp_z * sin(travelZ);
+      stars[i].mZ = temp_z * cos(travelZ) + temp_x * sin(travelZ);
+    }
+
+    stars[i].mScreenX = stars[i].mX / stars[i].mZ * 100 + SCREEN_WIDTH / 2;
+    stars[i].mScreenY = stars[i].mY / stars[i].mZ * 100 + SCREEN_HEIGHT / 2;
+
+    actualTime = Milliseconds() * .05;
+    if (mBoostSpeed && mCurrSpeed <= STAR_SPEED_MAX && actualTime - speedMills >= 25) {
+      mCurrSpeed = mCurrSpeed + 5;
+      speedMills = actualTime;
+    }
+
+    if (!mBoostSpeed && mCurrSpeed > STAR_SPEED_MIN && actualTime - speedMills >= 10) {
+      mCurrSpeed -= 5;
+      speedMills = actualTime;
+    }
+    if (mCurrSpeed < STAR_SPEED_MIN) {
+      mCurrSpeed = STAR_SPEED_MIN;
+    }
+
+
+    //If the stars go off the screen remove them and re-draw. If the stars hang out in the center remove them also
+    if (stars[i].mScreenX > SCREEN_WIDTH || stars[i].mScreenX < 0 || stars[i].mScreenY > SCREEN_HEIGHT ||
+        stars[i].mScreenY < 0 ||
+        (stars[i].mScreenX == SCREEN_WIDTH >> 1 && stars[i].mScreenY == SCREEN_HEIGHT >> 1)) {
+      int xMin = -1000,
+          xMax = 1000,
+          yMin = -500,
+          yMax = 500;
+
+      if ((gGame->GetState() == GAME_STATE_GAME) && (jsLeft || jsRight)) {
+        xMin = -1000;
+        xMax = 1000;
+      }
+
+      stars[i].Randomize(
+        xMin,
+        xMax,
+        yMin,
+        yMax,
+        RANDOM_Z_MIN,
+        RANDOM_Z_MAX,
+        STAR_SPEED_MIN,
+        STAR_SPEED_MAX
+      );
+
+      stars[i].mScreenX = stars[i].mX / stars[i].mZ * 100 + SCREEN_WIDTH / 2;
+      stars[i].mScreenY = stars[i].mY / stars[i].mZ * 100 + SCREEN_HEIGHT / 2;
+      stars[i].mOldScreenX = stars[i].mScreenX;
+      stars[i].mOldScreenY = stars[i].mScreenY;
+    }
+
+
+    // Draw the star at its new coordinate.
+    gDisplay.renderBitmap->DrawLine(
+        ENull,
+        stars[i].mScreenX,
+        stars[i].mScreenY,
+        stars[i].mOldScreenX,
+        stars[i].mOldScreenY,
+        STAR_COLOR
+    );
+
+    //keep track of the old spot
+    stars[i].mOldScreenX = stars[i].mScreenX;
+    stars[i].mOldScreenY = stars[i].mScreenY;
+  }
+
 }
--- Evade2/src/common/GStarfield.h
+++ Evade2/src/common/GStarfield.h
@@ -7,23 +7,112 @@
 
 #include <BPlayfield.h>
 
-const int NUM_STARS = 128;
+//
+//class GStarfield : public BPlayfield {
+//public:
+//  GStarfield();
+//
+//  ~GStarfield();
+//
+//public:
+//  void Render() OVERRIDE;
+//
+//protected:
+//  void InitStar(TInt aIndex);
+//
+//protected:
+//  TFloat mStarX[NUM_STARS], mStarY[NUM_STARS], mStarZ[NUM_STARS];
+//};
 
-class GStarfield : public BPlayfield {
+static const TInt16 NUM_STARS = 90;
+
+static TUint32 actualTime = 0;
+static TInt32 speedMills;
+
+static const TInt16 STAR_SPEED_MIN = 1; // Minimum movement in pixels per update. (value is inclusive)
+static const TInt16 STAR_SPEED_MAX = 15; // Maximum movement in pixels per update. (value is inclusive)
+
+static const TUint16 RANDOM_Z_MIN = 400;
+static const TUint16 RANDOM_Z_MAX = 600;
+
+
+class GStar {
 public:
-  GStarfield();
+    GStar() {
+      mX = 0;
+      mY = 0;
+      mZ = 0;
+      mSpeed = 0;
+      mScreenX = 0;
+      mScreenY = 0;
+      mOldScreenX = 0;
+      mOldScreenY = 0;
+    }
 
-  ~GStarfield();
-
-public:
-  void Render() OVERRIDE;
-
-protected:
-  void InitStar(TInt aIndex);
-
-protected:
-  TFloat mStarX[NUM_STARS], mStarY[NUM_STARS], mStarZ[NUM_STARS];
+    void Randomize(TInt16 xMin, TInt16 xMax, TInt16 yMin, TInt16 yMax, TInt16 zMin, TInt16 zMax, TInt16 speedMin, TInt16 speedMax)  {
+      mX = Random(xMin, xMax);
+      mY = Random(yMin, yMax);
+      mZ = Random(zMin, zMax);
+      mSpeed = Random(speedMin, speedMax + 1);
+    }
+    TFloat mX, mY, mZ, mSpeed;
+    TInt16 mScreenX, mScreenY, mOldScreenX, mOldScreenY;
 };
 
 
+
+static GStar stars[NUM_STARS];  // An array of star instances.
+static TUint8 colors[NUM_STARS];
+
+class GStarfield : public BPlayfield {
+public:
+    GStarfield();
+
+    ~GStarfield() = default;
+
+    void Render();
+
+    void Animate() {
+//    for (TInt i = 0; i < NUM_STARS; i++) {
+//      if (mStarX[i] < -10) {
+//        InitStar(i);
+//      }
+//      else {
+//        mStarX[i] -= mStarSpeed[i];
+//      }
+//    }
+    }
+    TBool mWarp;
+    TBool mBoostSpeed;
+
+protected:
+
+    void InitStar(TInt aIndex, TInt32 aXStartIndex = 319) {
+      stars[aIndex].Randomize(
+          -500,
+          500,
+          -500,
+          500,
+          0,
+          1,
+          STAR_SPEED_MIN,
+          STAR_SPEED_MAX
+      );
+
+    //  mStarX[aIndex] = TFloat(256) - Random(0, 512) + gCamera->x;
+    //  mStarY[aIndex] = TFloat(256) - Random(0, 512) + gCamera->y;
+    //  mStarZ[aIndex] = gCamera->z + Random(200, 512);
+//    colors[aIndex] = 255 - Random(0,128);
+
+    }
+
+//  TFloat mStarX[NUM_STARS]{};
+//  TFloat mStarY[NUM_STARS]{};
+//  TFloat mStarZ[NUM_STARS]{};
+//  TFloat mStarWidth[NUM_STARS]{};
+//  TFloat mStarSpeed[NUM_STARS]{};
+    TInt mCurrSpeed;
+    TInt mMinSpeed;
+
+};
 #endif //EVADE2_GSTARFIELD_H
